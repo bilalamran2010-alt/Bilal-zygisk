acece4bcb0006a54a8947b3f3a49199448fd31bd
Your Name <your@email.com>
2026-06-21 18:38:46 +0100

    Update

--- app/src/main/java/com/LOL/project/MenuService.kt
+++ app/src/main/java/com/LOL/project/MenuService.kt
@@ -3,6 +3,7 @@ package com.LOL.project
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.widget.Toast
 
 class MenuService : Service() {
     override fun onBind(intent: Intent?): IBinder? {
@@ -11,7 +12,12 @@ class MenuService : Service() {
 
     override fun onCreate() {
         super.onCreate()
-        System.loadLibrary("LOL")
+        try {
+            System.loadLibrary("LOL")
+            Toast.makeText(this, "Library Loaded Successfully", Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            Toast.makeText(this, "Library Load Failed: " + e.message, Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
