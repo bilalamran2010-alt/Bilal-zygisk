f54fd54ce0dab8595d8c899d0a1f425ac0782ab3
Your Name <your@email.com>
2026-06-21 05:14:50 +0100

    Update

---
+++ app/src/main/jni/Struct/Defines.h
@@ -0,0 +1,11 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+struct Config {
+    struct { bool enabled, all, rage, legit, silent; } aim;
+    struct { bool enabled, box, line, Health, enabledRgb; } esp;
+};
+
+typedef UnityEngine_Touch_Fields (*GetTouch_t)(int index);
+
+#endif
--- app/src/main/jni/Struct/main.h
+++ app/src/main/jni/Struct/main.h
@@ -5,29 +5,11 @@
 
 typedef UnityEngine_Touch_Fields (*GetTouch_t)(int index);
 
+extern Config cfg;
+extern GetTouch_t old_GetTouch;
 extern bool HasOriginalPos;
 extern bool fourFingerPressed;
 extern bool HideFullMenu;
 extern bool NewBox;
-
-struct Config {
-    struct {
-        bool enabled;
-        bool all;
-        bool rage;
-        bool legit;
-        bool silent;
-    } aim;
-
-    struct {
-        bool enabled;
-        bool box;
-        bool line;
-        bool Health;
-        bool enabledRgb;
-    } esp;
-};
-
-inline Config cfg; 
 
 #endif
