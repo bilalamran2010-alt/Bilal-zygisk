ff9587b70198587d6bc88c8c417f678300f12760
Your Name <your@email.com>
2026-06-21 18:14:52 +0100

    Update

--- app/src/main/java/com/LOL/project/MainActivity.kt
+++ app/src/main/java/com/LOL/project/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.LOL.project
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        
+        btnLogin.setOnClickListener {
+            // This intent starts the service that loads the cheat
+            val intent = Intent(this, MenuService::class.java)
+            startService(intent)
+            
+            // Close the app to show the menu over the game
+            finish() 
+        }
     }
 }
